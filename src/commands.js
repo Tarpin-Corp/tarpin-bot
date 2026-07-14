9b774ce7c3ecdfef2791223fb536aa90f83c6ed0
kwaaac <max.rodriguezidf@gmail.com>
2026-07-14 13:31:06 +0200

    LINT: applied linter to other files

--- src/commands.js
+++ src/commands.js
@@ -3,28 +3,28 @@ import { InstallGlobalCommands } from './utils.js';
 
 // Simple test command
 const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
+	name: 'test',
+	description: 'Basic command',
+	type: 1,
+	integration_types: [0, 1],
+	contexts: [0, 1, 2],
 };
 
 // Command containing options
 const ROLL_COMMAND = {
-  name: 'roll',
-  description: `Lance un dé comme sur Roll20 !`,
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-    },
-  ],
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 2],
+	name: 'roll',
+	description: 'Lance un dé comme sur Roll20 !',
+	options: [
+		{
+			type: 3,
+			name: 'object',
+			description: 'Pick your object',
+			required: true,
+		},
+	],
+	type: 1,
+	integration_types: [0, 1],
+	contexts: [0, 2],
 };
 
 
